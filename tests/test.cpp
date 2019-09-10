ebe91a204b61d34077b4af9f8838c3842cc9a0c7
Kirill <kirillpo2016@mail.ru>
2019-09-11 00:33:07 +0300

    deleted commets

--- tests/test.cpp
+++ tests/test.cpp
@@ -3,18 +3,18 @@
 #include <gtest/gtest.h>
 #include <header.hpp>
 
-Json a(
-    "{\"response\":[{\"id\":210700286,\"first_name\":\"Lindsey\",\"last_name\":"
-    "\"Stirling\"}]}");
+
+
+
 TEST(circuits, first) {
   Json a(
       "{\"lastname\":\"Ivanov\",\"firstname\":\"Ivan\",\"age\":25,\"islegal\":"
       "false,\"marks\":[4,5,5,5,2,3],\"address\":{\"city\":\"Moscow\","
-      "\"street\":\"Vozdvijenka\"}}")
+      "\"street\":\"Vozdvijenka\"}}");
 
-      EXPECT_EQ(any_cast<string>(a["lastname"]), "Ivanov");
-  EXPECT_EQ(any_cast<string>(a["firstname"]), "Ivan");
-  EXPECT_EQ(any_cast<string>(a["islegal"]), false);
+  EXPECT_EQ(any_cast<std::string>(a["lastname"]), "Ivanov");
+  EXPECT_EQ(any_cast<std::string>(a["firstname"]), "Ivan");
+  EXPECT_EQ(any_cast<std::string>(a["islegal"]), false);
   EXPECT_EQ(any_cast<int>(std::any_cast<Json>(a["marks"])[0]), 4);
   EXPECT_EQ(any_cast<int>(std::any_cast<Json>(a["marks"])[1]), 5);
   EXPECT_EQ(any_cast<int>(std::any_cast<Json>(a["marks"])[2]), 5);
