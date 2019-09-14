9d3be6d72aead6e95e673a86c9f59884b0f52e41
Kirill <kirillpo2016@mail.ru>
2019-09-14 21:18:29 +0300

    New code,new life

--- include/header.hpp
+++ include/header.hpp
@@ -3,14 +3,18 @@
 #define INCLUDE_HEADER_HPP_
 #include <stdlib.h>
 #include <any>
+#include <exception>
+#include <fstream>
 #include <iostream>
 #include <map>
 #include <string>
 #include <vector>
 
+using namespace std;
 std::string copy_figure_skobka(const std::string& s, size_t& index) {
   std::string x;
   size_t k = 1;
+  x += s[index];
   index++;
   bool flag = true;
   while (flag) {
@@ -24,21 +28,35 @@ std::string copy_figure_skobka(const std::string& s, size_t& index) {
       if (k == 0) flag = false;
     }
   }
+  x += s[index];
   index++;
   return x;
 }
 
-std::string copy_chislo(const std::string& s, size_t& index) {
+std::string copy_chislo(const std::string& s, size_t& index, bool& dble) {
+  dble = false;
   std::string x;
-  while (s[index] > 47 && s[index] < 58) {
+  if (s[0] == '.') throw std::exception();
+  int flag = 0;
+  while ((s[index] > 47 && s[index] < 58) || s[index] == '.') {
+    if (s[0] == '.') {
+      flag++;
+    }
     x += s[index];
     index++;
+  }
+  if (flag == 1) {
+    dble = true;
+  }
+  if (flag > 1) {
+    throw std::exception();
   }
   return x;
 }
 
 std::string copy_kavichki(const std::string& s, size_t& index) {
   std::string x;
+  size_t k = 1;
   index++;
   bool flag = true;
   while (flag) {
@@ -55,6 +73,7 @@ std::string copy_kavichki(const std::string& s, size_t& index) {
 std::string copy_kvadrat_skobka(const std::string& s, size_t& index) {
   std::string x;
   size_t k = 1;
+  x += s[index];
   index++;
   bool flag = true;
   while (flag) {
@@ -68,183 +87,166 @@ std::string copy_kvadrat_skobka(const std::string& s, size_t& index) {
       if (k == 0) flag = false;
     }
   }
+  x += s[index];
   index++;
   return x;
+}
+
+std::string read_from_file(const string& path) {
+  std::string str;
+  std::string line;
+  std::ifstream fin(path);
+  if (fin.is_open()) {
+    while (getline(fin, line)) {
+      str += line;
+    }
+  }
+  fin.close();
+  std::string m;
+  size_t i = 0;
+  while (i < str.length()) {
+    if (str[i] != ' ' && str[i] != '	' && str[i] != '\n') m += str[i];
+    i++;
+  }
+  return str;
 }
 
 class Json {
  public:
   Json() = default;
-
-  Json object(const std::string& s) {
-    std::string x;
-    std::string info;
-    std::string key;
-    size_t i = 0;
-    Json a;
-    while (i < s.length()) {
-      if (s[i] == '\"') {
-        if (key == "") {
-          key = copy_kavichki(s, i);
-          i++;
-          if (s[i] == '\"') {
-            info = copy_kavichki(s, i);
-            a.data.emplace(key, info);
-          }
-          if (s[i] == 't') {
-            a.data.emplace(key, true);
-          }
-          if (s[i] == 'f') {
-            a.data.emplace(key, false);
-          }
-          if (s[i] > 47 && s[i] < 58) {
-            int n = 0;
-            info = copy_chislo(s, i);
-            n = stoi(info);
-            a.data.emplace(key, n);
-          }
-          if (s[i] == '{') {
-            x = copy_figure_skobka(s, i);
-            a.data.emplace(key, object(x));
-          }
-          if (s[i] == '[') {
-            x = copy_kvadrat_skobka(s, i);
-            a.data.emplace(key, mass(x));
-          }
-          key = "";
-        }
+  Json(const std::string& s) {
+    std::string str;
+    size_t k = 0;
+    while (k < s.length()) {
+      if (s[k] == ' ' || s[k] == '	' || s[k] == '\n') {
+        k++;
+      } else {
+        str += s[k];
+        k++;
       }
-      i++;
     }
-    return a;
-  }
-
-  Json mass(const std::string& s) {
-    std::string x;
-    std::string info;
-    size_t i = 0;
-    Json a;
-    std::vector<std::any> arr;
-    while (i < s.length()) {
-      if (s[i] == '\"') {
-        info = copy_kavichki(s, i);
-        a.ar.push_back(info);
-      }
-      if (s[i] == 't') {
-        i += 4;
-        a.ar.push_back(true);
-      }
-      if (s[i] == 'f') {
-        a.ar.push_back(false);
-        i += 5;
-      }
-      if (s[i] > 47 && s[i] < 58) {
-        int n = 0;
-        info = copy_chislo(s, i);
-        n = stoi(info);
-        a.ar.push_back(n);
-      }
-      if (s[i] == '{') {
-        x = copy_figure_skobka(s, i);
-        a.ar.push_back(object(x));
-      }
-      if (s[i] == '[') {
-        x = copy_kvadrat_skobka(s, i);
-        a.ar.push_back(mass(x));
-      }
-      i++;
-    }
-    return a;
-  }
-  explicit Json(const std::string& s) {
-    if (s[0] == '{') {
-      std::string x;
-      std::string info;
-      std::string key;
+    // cout << str;
+    if (str[0] != '{' && str[0] != '[') throw std::exception();
+    if (str[0] == '{') {
+      size_t m = 0;
       size_t i = 0;
-      Json a;
-      while (i < s.length()) {
-        if (s[i] == '\"') {
-          if (key == "") {
-            key = copy_kavichki(s, i);
+      i++;
+      std::string key;
+      std::string x;
+      x = str;
+      while (i < x.length()) {
+        if (x[i] != '\"')
+          throw std::exception();
+        else {
+          key = copy_kavichki(x, i);
+          if (x[i] != ':')
+            throw std::exception();
+          else {
             i++;
-            if (s[i] == '\"') {
-              info = copy_kavichki(s, i);
+            if (x[i] == '\"') {
+              std::string info;
+              info = copy_kavichki(x, i);
               data.emplace(key, info);
             }
-            if (s[i] == 't') {
+            if (x[i] == 't') {
               data.emplace(key, true);
+              i += 4;
             }
-            if (s[i] == 'f') {
+            if (x[i] == 'f') {
               data.emplace(key, false);
+              i += 5;
             }
-            if (s[i] > 47 && s[i] < 58) {
-              int n = 0;
-              info = copy_chislo(s, i);
-              n = stoi(info);
-              data.emplace(key, n);
+            if (x[i] > 47 && x[i] < 58) {
+              std::string info;
+              bool flag;
+              info = copy_chislo(x, i, flag);
+              if (flag) {
+                double a;
+                a = stof(info);
+                data.emplace(key, a);
+              } else {
+                int a;
+                a = stoi(info);
+                data.emplace(key, a);
+              }
             }
-            if (s[i] == '{') {
-              x = copy_figure_skobka(s, i);
-              data.emplace(key, object(x));
+            if (x[i] == '{') {
+              std::string m;
+              m = copy_figure_skobka(x, i);
+              data.emplace(key, Json(m));
             }
-            if (s[i] == '[') {
-              x = copy_kvadrat_skobka(s, i);
+            if (x[i] == '[') {
+              std::string m;
+              m = copy_kvadrat_skobka(x, i);
+              data.emplace(key, Json(m));
+            }
 
-              data.emplace(key, mass(x));
+            if (str[i] != ',' && str[i] != '}' && str[i] != ']' &&
+                i != str.length())
+              throw std::exception();
+            else {
+              i++;
+              key = "";
+              continue;
             }
-            key = "";
           }
         }
-        i++;
       }
     }
-    if (s[0] == '[') {
-      std::string x;
-      std::string info;
+    if (str[0] == '[') {
+      std::string x = s;
       size_t i = 1;
-      Json a;
-      std::vector<std::any> arr;
-      while (i < s.length()) {
-        if (s[i] == '\"') {
-          info = copy_kavichki(s, i);
-          ar.push_back(info);
+      x = str;
+      while (i < x.length()) {
+        if (x[i] == '\"') {
+          std::string info;
+          info = copy_kavichki(x, i);
         }
-        if (s[i] == 't') {
+        if (x[i] == 't') {
           i += 4;
           ar.push_back(true);
         }
-        if (s[i] == 'f') {
+        if (x[i] == 'f') {
           ar.push_back(false);
           i += 5;
         }
-        if (s[i] > 47 && s[i] < 58) {
-          int n = 0;
-          info = copy_chislo(s, i);
-          n = stoi(info);
-          ar.push_back(n);
+        if (x[i] > 47 && x[i] < 58) {
+          std::string info;
+          bool flag;
+          info = copy_chislo(x, i, flag);
+          if (flag) {
+            double a;
+            a = stof(info);
+            ar.push_back(a);
+          } else {
+            int a;
+            a = stoi(info);
+            ar.push_back(a);
+          }
+          if (x[i] == '{') {
+            std::string m;
+            m = copy_figure_skobka(x, i);
+            ar.push_back(Json(m));
+          }
+          if (x[i] == '[') {
+            std::string m;
+            m = copy_kvadrat_skobka(x, i);
+            ar.push_back(Json(x));
+          }
+          if (str[i] != ',' && str[i] != '}' && str[i] != ']' &&
+              i != str.length())
+            throw std::exception();
+          else {
+            i++;
+            continue;
+          }
         }
-        if (s[i] == '{') {
-          x = copy_figure_skobka(s, i);
-          ar.push_back(object(x));
-        }
-        if (s[i] == '[') {
-          x = copy_kvadrat_skobka(s, i);
-          ar.push_back(mass(x));
-        }
-        i++;
       }
     }
   }
 
-  /*void vivod() const
-  {
-          for (size_t i = 0; i < ar.size(); i++)
-          {
-                  std::cout << any_cast<int>(ar[i]);
-          }
-  }*/
-
+  // Метод возвращает true, если данный экземпляр содержит в себе JSON-массив.
+  // Иначе false.
   bool is_array() const { return ar.size(); }
 
   bool is_object() const { return data.size(); }
@@ -252,11 +254,15 @@ class Json {
   std::any& operator[](const std::string& key) { return data[key]; }
 
   std::any& operator[](int index) { return ar[index]; }
-
   static Json parse(const std::string& s) { return Json(s); }
+  static Json parseFile(const std::string& path_to_file) {
+    s = read_from_file(path_to_file);
+    return Json(s)
+  }
 
  private:
   std::map<std::string, std::any> data;
   std::vector<std::any> ar;
 };
+
 #endif  // INCLUDE_HEADER_HPP_
