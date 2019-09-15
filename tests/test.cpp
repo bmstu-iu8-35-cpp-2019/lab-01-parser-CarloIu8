35e7d870e0c4f27bffc7f582332fabe109cc538b
Kirill <kirillpo2016@mail.ru>
2019-09-15 11:03:02 +0300

    path to file

--- tests/test.cpp
+++ tests/test.cpp
@@ -3,9 +3,46 @@
 #include <gtest/gtest.h>
 #include <header.hpp>
 
-TEST(circuits, first) {
-  Json a;
-  a.parseFile("../file.json");
+TEST(konstrukt, first) {
+  Json a(
+      "{\"lastname\":\"Ivanov\",\"firstname\":\"Ivan\",\"age\":25,\"islegal\":"
+      "false,\"marks\":[4,5,5,5,2,3],\"address\":{\"city\":\"Moscow\","
+      "\"street\":\"Vozdvijenka\"}}");
+  EXPECT_EQ(std::any_cast<std::string>(a["lastname"]), "Ivanov");
+  EXPECT_EQ(std::any_cast<std::string>(a["firstname"]), "Ivan");
+  EXPECT_EQ(std::any_cast<bool>(a["islegal"]), false);
+  EXPECT_EQ(std::any_cast<int>(std::any_cast<Json>(a["marks"])[0]), 4);
+  EXPECT_EQ(std::any_cast<int>(std::any_cast<Json>(a["marks"])[1]), 5);
+  EXPECT_EQ(std::any_cast<int>(std::any_cast<Json>(a["marks"])[2]), 5);
+  EXPECT_EQ(std::any_cast<int>(std::any_cast<Json>(a["marks"])[3]), 5);
+  EXPECT_EQ(std::any_cast<int>(std::any_cast<Json>(a["marks"])[4]), 2);
+  EXPECT_EQ(std::any_cast<int>(std::any_cast<Json>(a["marks"])[5]), 3);
+  EXPECT_EQ(std::any_cast<std::string>(std::any_cast<std::string>(
+                std::any_cast<Json>(a["address"])["city"])),
+            "Moscow");
+}
+
+TEST(parse, first) {
+  Json a = Json::parse(
+      "{\"lastname\":\"Ivanov\",\"firstname\":\"Ivan\",\"age\":25,\"islegal\":"
+      "false,\"marks\":[4,5,5,5,2,3],\"address\":{\"city\":\"Moscow\","
+      "\"street\":\"Vozdvijenka\"}}");
+  EXPECT_EQ(std::any_cast<std::string>(a["lastname"]), "Ivanov");
+  EXPECT_EQ(std::any_cast<std::string>(a["firstname"]), "Ivan");
+  EXPECT_EQ(std::any_cast<bool>(a["islegal"]), false);
+  EXPECT_EQ(std::any_cast<int>(std::any_cast<Json>(a["marks"])[0]), 4);
+  EXPECT_EQ(std::any_cast<int>(std::any_cast<Json>(a["marks"])[1]), 5);
+  EXPECT_EQ(std::any_cast<int>(std::any_cast<Json>(a["marks"])[2]), 5);
+  EXPECT_EQ(std::any_cast<int>(std::any_cast<Json>(a["marks"])[3]), 5);
+  EXPECT_EQ(std::any_cast<int>(std::any_cast<Json>(a["marks"])[4]), 2);
+  EXPECT_EQ(std::any_cast<int>(std::any_cast<Json>(a["marks"])[5]), 3);
+  EXPECT_EQ(std::any_cast<std::string>(std::any_cast<std::string>(
+                std::any_cast<Json>(a["address"])["city"])),
+            "Moscow");
+}
+
+TEST(konstrukt, first) {
+  Json a = Json::parseFile("../file.json");
   EXPECT_EQ(std::any_cast<std::string>(a["lastname"]), "Ivanov");
   EXPECT_EQ(std::any_cast<std::string>(a["firstname"]), "Ivan");
   EXPECT_EQ(std::any_cast<bool>(a["islegal"]), false);
